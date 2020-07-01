72a9d260b119742ca26dab8d2d03f2a3890a9639
Rama <mramamaulana60@gmail.com>
2020-07-01 09:20:26 +0700

    testing webpack-dev-server

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html")
+    })
+  ],
+  output: {
+    path: path.resolve(__dirname, "build")
+  }
+};
